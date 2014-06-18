6f901ae4438b01043741558db647315366fae317
Saad Tazi <saad@saadtazi.com>
2014-06-17 22:40:46 -0400

    initial commit. still missing codebook

---
+++ README.md
@@ -0,0 +1,44 @@
+# Introduction
+
+This repository contains R scripts to merge and tidy data collected from an experiment that was collecting data from the accelerometers Samsung Galaxy S smartphones.
+
+It was a project done during the "Getting and cleaning data" coursera course (started June 2nd, 2014).
+
+# Data source
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+For a description of the data source, check README.txt and features_info.txt contained in the zip file.
+
+# Steps
+
+1. Clone this repository
+2. change the working directory to getting-cleaning-data-project/ folder
+3. Download the data source zip file
+4. Unzip the source zip file: `unzip getdata-projectfiles-UCI\ HAR\ Dataset.zip`
+5. Run run_analysis.R
+
+Corresponding commands are:
+
+```
+git clone https://github.com/saadtazi/getting-cleaning-data-project
+cd getting-cleaning-data-project
+wget https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+unzip getdata-projectfiles-UCI\ HAR\ Dataset.zip
+R run_analysis.R
+```
+
+***These commands have been tested on Mac OSX and might have to be adapted for other operating systems.***
+
+# Output
+
+run_analysis.R generates 2 files:
+* tidy_data.txt: a space-delimited value file that contains mean and standard deviation for each measurements from the train and test data,
+* average_data.txt: a space-delimited value file that contains tidy data set with the average of each variable for each activity and each subject
+
+I used default params for write.table() so space-delimited, with column names, ... To read those files, use:
+
+```
+read.table('tidy_data.txt')
+read.table('average_data')
+```
---
+++ run_analysis.R
@@ -0,0 +1,81 @@
+# change me if needed - this folder is created when unzipping data source into the repo
+dataFolder = "UCI HAR Dataset/"
+
+getFilePath <- function(filepath) {
+  paste(dataFolder, filepath, sep="")
+  
+}
+
+# First: Merge training and the test files: 
+# - X
+# - Y
+# - subject
+
+XTrain <- read.table(getFilePath("train/X_train.txt"))
+XTest <- read.table(getFilePath("test/X_test.txt"))
+XData <- rbind(XTrain, XTest)
+# be a good citizen and clean vars that are no longer required
+rm(XTrain, XTest)
+
+YTrain <- read.table(getFilePath("train/y_train.txt"))
+YTest <- read.table(getFilePath("test/y_test.txt"))
+YData <- rbind(YTrain, YTest)
+# ... good citizen...
+rm(YTrain, YTest)
+
+subjectTrain <- read.table(getFilePath("train/subject_train.txt"))
+subjectTest <- read.table(getFilePath("test/subject_test.txt"))
+SubjectData <- rbind(subjectTrain, subjectTest)
+# ... still a good citizen
+rm(subjectTrain, subjectTest)
+
+# #2: We should only keep mean and standard deviation for each measurement.
+
+features <- read.table(getFilePath("features.txt"))
+keptMesurePositions <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
+XData <- XData[, keptMesurePositions]
+
+# we set and clean column names
+names(XData) <- features[keptMesurePositions, 2]
+# we remove unwanted characters from column names
+names(XData) <- gsub("\\(|\\)", "", names(XData))
+names(XData) <- tolower(names(XData))
+# time to cleanup no longer required variables
+rm(keptMesurePositions, features)
+
+# #3: have a descriptive activity column, based on activity_labels.txt
+activityLabels <- read.table(getFilePath("activity_labels.txt"))
+# clean activity names (one line this time!)
+activityLabels[, 2] = gsub("_", "", tolower(as.character(activityLabels[, 2])))
+YData[,1] = activityLabels[YData[,1], 2]
+# name that new column
+names(YData) <- "activity"
+
+# label SubjectData
+names(SubjectData) <- "subjectnumber"
+
+# Then: Time to merge everthing together and save into a file: XData, YData, SubjectData
+
+merged <- cbind(SubjectData, YData, XData)
+write.table(merged, "tidy_data.txt")
+
+# #5: "Creates a 2nd data set with the average of each variable for each activity and each subject."
+
+subjects <- unique(SubjectData)[,1]
+nbSubjects <- length(subjects)
+nbActivities <- length(activityLabels[,1])
+nbCols <- dim(merged)[2]
+result <- merged[1:(nbSubjects*nbActivities), ]
+
+# TODO try to use functional programming functions (apply...)
+row = 1
+for (s in 1:nbSubjects) {
+  for (a in 1:nbActivities) {
+    result[row, 1] = subjects[s]
+    result[row, 2] = activityLabels[a, 2]
+    tmp <- merged[merged$subject==s & merged$activity==activityLabels[a, 2], ]
+    result[row, 3:nbCols] <- colMeans(tmp[, 3:nbCols])
+    row = row+1
+  }
+}
+write.table(result, "average_data.txt")
